ce4a0bf8ffc5911640c796a01587eb90211f2ba2
adhSwede <119529943+adhSwede@users.noreply.github.com>
2025-06-11 15:12:14 +0200

    Added in two more projects

--- src/pages/Projects/projectData.ts
+++ src/pages/Projects/projectData.ts
@@ -14,6 +14,21 @@ const projects: ProjectCardProps[] = [
     stack: ["Java", "Spring Boot", "JPA"],
     githubUrl: "https://github.com/adhSwede/spring-library-mariadb",
   },
+  {
+  title: "Free is Fun",
+  description: "A free game browser using the FreeToGame API. Built with React, styled with Tailwind",
+  stack: ["Vite", "React", "React Router", "Tailwind CSS", "Material Icons"],
+  githubUrl: "https://github.com/adhSwede/free-is-fun",
+  liveUrl: "https://free-is-fun.vercel.app/"
+  },
+  {
+  title: "Superhero Project",
+  description:
+    "A collaborative React app built with a classmate on our own time. Fetches and displays superhero data using the Superhero API.",
+  stack: ["React", "React Router", "Tailwind CSS", "Superhero API"],
+  githubUrl: "https://github.com/WictorNisa/superhero-project",
+}
+
 ];
 
 export default projects;
